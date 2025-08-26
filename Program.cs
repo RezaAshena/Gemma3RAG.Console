3f7b1ceed71e36aa6b6e2462b26f338c2c99d265
Reza Ashena <Ashena.reza@gmail.com>
2025-08-26 14:23:21 -0400

    Add Ollama integration and update budget data
    Updated `Program.cs` to include `Microsoft.KernelMemory` and `Microsoft.KernelMemory.AI.Ollama` namespaces. Configured Ollama service for text and embedding generation, and implemented document import and question answering functionality with user prompts and console output.
    
    Populated `budget.txt` with detailed expense entries for analysis.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using Microsoft.KernelMemory;
+using Microsoft.KernelMemory.AI.Ollama;
+
+var config = new OllamaConfig
+{
+    Endpoint = "http://localhost:11434",
+    TextModel = new OllamaModelConfig("deepseek-r1:1.5b", 131072),
+    EmbeddingModel = new OllamaModelConfig("deepseek-r1:1.5b", 2048),
+};
+
+var memory = new KernelMemoryBuilder()
+    .WithOllamaTextGeneration(config)
+    .WithOllamaTextEmbeddingGeneration(config)
+    .Build<MemoryServerless>();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Importing and processing  document, please wait...");
+Console.ResetColor();
+
+await memory.ImportDocumentAsync("budget.txt", documentId: "DOC001");
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Model is ready to take questions\n");
+
+while (await memory.IsDocumentReadyAsync("DOC001"))
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("... Ask your questions....\n");
+    Console.ResetColor();
+
+    var question = Console.ReadLine();
+    var answer = await memory.AskAsync(question);
+
+    Console.WriteLine(answer.Result);
+
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("\n Sourcess: ");
+    foreach (var source in answer.RelevantSources)
+    {
+        Console.WriteLine($" {source.SourceName} - {source.SourceUrl} - {source.Link}");
+    }
+    Console.ResetColor();
+}
